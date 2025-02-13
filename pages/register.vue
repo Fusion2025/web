e8dabe2373b9a4a6f389cc343a48b8cd4d864d33
Pedro <pedro@boech.at>
2025-02-13 16:26:13 -0300

    feat: update registration

--- pages/register.vue
+++ pages/register.vue
@@ -6,11 +6,49 @@
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col class="page-content my-4 text-center" cols="12">
-        The registration system will be available once the lists of tutorials,
-        events, special sessions and others that affect individual registrations
-        is finalized. We expect this to happen on or before March 1st.
+      <v-col cols="12">
+        <h2 class="page-title">Registration Fees</h2>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-table class="text-center fee-table">
+          <thead>
+            <tr>
+              <th id="0" class="text-center"><b>Ticket type</b></th>
+              <th id="1" class="text-center"><b>Until May 15th</b></th>
+              <th id="2" class="text-center"><b>After May 15th</b></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>FULL</td>
+              <td>USD 675.00</td>
+              <td>USD 775.00</td>
+            </tr>
+            <tr>
+              <td>STUDENT</td>
+              <td>USD 395.00</td>
+              <td>USD 495.00</td>
+            </tr>
+            <tr>
+              <td>SINGLE TUTORIAL</td>
+              <td>USD 300.00</td>
+              <td>USD 300.00</td>
+            </tr>
+            <tr>
+              <td>BOOT CAMP</td>
+              <td>USD 100.00</td>
+              <td>USD 100.00</td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style>
+.fee-table td,
+.fee-table th {
+  font-size: x-large;
+}
+</style>
