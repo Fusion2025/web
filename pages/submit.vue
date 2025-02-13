f01dc096dc0f9680bcf8f2b39ebbb98df7db006d
Pedro <pedro@boech.at>
2025-02-13 15:56:14 -0300

    feat: update submit page

--- pages/submit.vue
+++ pages/submit.vue
@@ -6,11 +6,33 @@
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col class="page-content my-4 text-center" cols="12">
-        The submission system will be made available as soon as the list of
-        approved special sessions is finalized. We expect this to happen on or
-        before March 1st.
+      <v-col cols="12">
+        <v-alert
+          class="text-center submission-link"
+          variant="tonal"
+          color="red"
+        >
+          <p class="text-center pl-4">
+            Please submit your original paper for FUSION 2025 using this CMT
+            link:
+          </p>
+          <p>
+            <NuxtLink
+              to="https://cmt3.research.microsoft.com/FUSION2025/Submission/Index"
+              target="_blank"
+            >
+              https://cmt3.research.microsoft.com/FUSION2025/Submission/Index</NuxtLink
+            >
+          </p>
+        </v-alert>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style>
+.submission-link p {
+  font-size: xx-large;
+  font-weight: bold;
+}
+</style>
