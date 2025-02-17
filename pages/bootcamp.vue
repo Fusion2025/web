4308132dea0558d0c29354f2e07f7393f057f1c5
Pedro Boechat <pedro@boech.at>
2025-02-17 15:09:36 -0300

    feat: add bootcamp

--- pages/bootcamp.vue
+++ pages/bootcamp.vue
@@ -6,9 +6,237 @@
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col class="page-content my-4 text-center" cols="12">
-        More details soon.
+      <v-col cols="12">
+        <h2 class="page-title">Schedule</h2>
+        <v-row no-gutters justify="center">
+          <v-col cols="12" md="6">
+            <v-card class="px-4 pb-4 mt-4">
+              <v-card-title class="px-0 card-title">
+                <h2>Monday, July 7th</h2>
+              </v-card-title>
+              <v-table class="text-center schedule-table overflow-visible">
+                <colgroup>
+                  <col span="1" class="schedule-col-1" />
+                  <col span="1" class="schedule-col-2" />
+                </colgroup>
+                <tbody>
+                  <tr>
+                    <td class="td-1">10:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/erik-blasch.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>Introduction to Information Fusion</b>
+                          <p>Erik Blasch</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">11:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/dale-blair.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>Estimation in Information Fusion</b>
+                          <p>Dale Blair</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">12:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/yuxuan-xia.jpeg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>Data Association</b>
+                          <p>Yuxuan Xia</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">13:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        />
+                        <v-col cols="12" md="6" lg="8">
+                          <p>Lunch</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">14:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/felix-govaers.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>Distributed Target Tracking</b>
+                          <p>Felix Govaers</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">15:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/yaakov-bar-shalom.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>
+                            Fusion and Association with Attributes /
+                            Classification</b
+                          >
+                          <p>Yaakov Bar-Shalom</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">16:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/pramod-varshney.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b> Distributed Inference and Information Fusion </b>
+                          <p>Pramod Varshney</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="td-1">17:00</td>
+                    <td class="td-1">
+                      <v-row align="center">
+                        <v-col
+                          class="profile-picture-container"
+                          cols="12"
+                          md="6"
+                          lg="4"
+                        >
+                          <img
+                            class="profile-picture"
+                            src="/img/bootcamp/erik-blasch.jpg"
+                          />
+                        </v-col>
+                        <v-col cols="12" md="6" lg="8">
+                          <b>High-Level Fusion</b>
+                          <p>Erik Blasch</p>
+                        </v-col>
+                      </v-row>
+                    </td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style>
+.schedule-table td {
+  font-size: x-large;
+}
+.schedule-table td:first-child {
+  border-right-width: thin !important;
+}
+.schedule-col-1 {
+  width: 20%;
+}
+.schedule-col-2 {
+  width: 80%;
+}
+.td-1 {
+  border: 0px solid rgba(var(--v-border-color), var(--v-border-opacity)) !important;
+  border-top-width: thin !important;
+  border-bottom-width: thin !important;
+}
+.td-n {
+  border: 0px solid rgba(var(--v-border-color), var(--v-border-opacity)) !important;
+  border-bottom-width: thin !important;
+}
+.calendar-link {
+  color: #005047;
+}
+.card-title h2 {
+  text-align: start;
+}
+.profile-picture {
+  width: 100%;
+  height: auto;
+  max-height: 150px;
+  max-width: 150px;
+  display: block;
+  margin: 5px auto;
+}
+</style>
