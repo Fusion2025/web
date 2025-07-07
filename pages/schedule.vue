4f999da327cdfc454ad41a728276277f5af7c6e9
Fusion2025 <fusion2025@labnet.nce.ufrj.br>
2025-07-07 11:52:28 -0300

    Update schedule.vue - revista upadted

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -6,7 +6,7 @@
         <h1 class="page-title" style="color: red">
           You will be able to download the detailed schedule at
           <a
-            href="https://drive.google.com/file/d/1Q7XnNLjloHl85I8ChNrhtBWxw5AHGnSU/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bAnO7BBf2HfceCrgsliPKk4B2oW14Ftk/view?usp=sharing"
           >
             this link</a
           >.
